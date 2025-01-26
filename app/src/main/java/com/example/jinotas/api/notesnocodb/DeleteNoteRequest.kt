037a0fea5a924d12cb7a96cf129dbbc07495b921
Jironiah <seandertal@gmail.com>
2025-01-26 03:35:46 +0100

    Removed due to deprecation

--- app/src/main/java/com/example/jinotas/api/notesnocodb/DeleteNoteRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.example.jinotas.api.notesnocodb
-
-import com.google.gson.annotations.SerializedName
-
-data class DeleteNoteRequest(
-    @SerializedName("Id") val Id: Int
-    )
--- app/src/main/java/com/example/jinotas/api/tokenusernocodb/ApiTokenUser.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jinotas.api.tokenusernocodb
-
-import com.google.gson.annotations.SerializedName
-
-data class ApiTokenUser(
-    @SerializedName("userName") val userName: String,
-    @SerializedName("token") val token: String
-)
