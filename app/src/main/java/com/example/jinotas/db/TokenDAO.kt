ba8fbc9c9e86992be14922eb06af7e33cf02f30d
Jironiah <seandertal@gmail.com>
2025-01-26 03:36:41 +0100

    Class changed to be equal to api

--- app/src/main/java/com/example/jinotas/db/TokenDAO.kt
+++ app/src/main/java/com/example/jinotas/db/TokenDAO.kt
@@ -9,11 +9,14 @@ import androidx.room.Update
 @Dao
 interface TokenDAO {
 
+    @Query("SELECT * FROM token")
+    fun getUserToken(): Token
+
     @Query("SELECT token FROM token")
     fun getToken(): String
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertNote(token: Token)
+    fun insertToken(token: Token)
 
     @Update
     fun updateToken(token: Token)
---
+++ app/src/main/java/com/example/jinotas/db/UserToken.kt
@@ -0,0 +1,7 @@
+package com.example.jinotas.db
+
+data class UserToken(
+    val token: String,
+    val userName: String,
+    val password: String
+)
