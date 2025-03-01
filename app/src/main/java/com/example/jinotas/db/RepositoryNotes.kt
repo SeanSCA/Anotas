2504b19ab13abc9c2cc9f57b07440465b476e28b
Jironiah <seandertal@gmail.com>
2025-03-01 14:21:26 +0100

    Created RepositoryNotes to add the main db functions that viewmodel uses

---
+++ app/src/main/java/com/example/jinotas/db/RepositoryNotes.kt
@@ -0,0 +1,26 @@
+package com.example.jinotas.db
+
+import androidx.lifecycle.LiveData
+
+class RepositoryNotes(private val noteDAO: NoteDAO) {
+
+    fun getNoteOrderByDate(): ArrayList<Note> {
+        return noteDAO.getNoteOrderByDate() as ArrayList<Note>
+    }
+
+    fun getNoteOrderByTitle(): ArrayList<Note> {
+        return noteDAO.getNoteOrderByTitle() as ArrayList<Note>
+    }
+
+    fun getNotesList(): ArrayList<Note> {
+        return noteDAO.getNotesList() as ArrayList<Note>
+    }
+
+    fun getNoteByTitle(filter: String): ArrayList<Note> {
+        return noteDAO.getNoteByTitle(filter) as ArrayList<Note>
+    }
+
+    fun getAllNotesLive(): LiveData<ArrayList<Note>>{
+        return noteDAO.getAllNotesLive() as LiveData<ArrayList<Note>>
+    }
+}
